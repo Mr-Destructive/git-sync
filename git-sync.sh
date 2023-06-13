60906f41dcb5a223010303cc265522c6c1c659c8
mr_destructive <gormeet711@gmail.com>
2023-06-13 13:44:06 +0530

    fix release 1

--- git-sync.sh
+++ git-sync.sh
@@ -7,17 +7,20 @@ SOURCE_BRANCH=$2
 DESTINATION_REPO=$3
 DESTINATION_BRANCH=$4
 
-if ! echo $SOURCE_REPO | grep -Eq ':|@|\.git\/?$'; then
-  if [[ -n "$SSH_PRIVATE_KEY" || -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
+if ! echo $SOURCE_REPO | grep '.git'
+then
+  if [[ -n "$SSH_PRIVATE_KEY" ]]
+  then
     SOURCE_REPO="git@github.com:${SOURCE_REPO}.git"
     GIT_SSH_COMMAND="ssh -v"
   else
     SOURCE_REPO="https://github.com/${SOURCE_REPO}.git"
   fi
 fi
-
-if ! echo $DESTINATION_REPO | grep -Eq ':|@|\.git\/?$'; then
-  if [[ -n "$SSH_PRIVATE_KEY" || -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
+if ! echo $DESTINATION_REPO | grep -E '.git|@'
+then
+  if [[ -n "$SSH_PRIVATE_KEY" ]]
+  then
     DESTINATION_REPO="git@github.com:${DESTINATION_REPO}.git"
     GIT_SSH_COMMAND="ssh -v"
   else
@@ -25,16 +28,10 @@ if ! echo $DESTINATION_REPO | grep -Eq ':|@|\.git\/?$'; then
   fi
 fi
 
-echo "SOURCE=${SOURCE_REPO}:${SOURCE_BRANCH}"
-echo "DESTINATION=${DESTINATION_REPO}:${DESTINATION_BRANCH}"
+echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
+echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
-if [[ -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
-  # Clone using source ssh key if provided
-  git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
-else
-  git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
-fi
-
+git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
 git remote add destination "$DESTINATION_REPO"
 
 # Pull all branches references down locally so subsequent commands can see them
@@ -42,10 +39,5 @@ git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 
 # Print out all branches
 git --no-pager branch -a -vv
-
-if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
-  # Push using destination ssh key if provided
-  git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
-fi
 
 git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
